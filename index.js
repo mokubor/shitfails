e1970db2a405004122f6f3c27db7d5fb67d85b7f
Miracle Okubor <mokubor@users.noreply.github.com>
2017-07-19 09:56:58 +0100

    Add files via upload
    chatbot file

---
+++ index.js
@@ -0,0 +1,110 @@
+'use strict'
+
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const messages = require('./messages');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 6080));
+
+//Allows us to process the data
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//read config values from json file
+var config_file = fs.readFileSync('./config/default.json');
+var config_values = JSON.parse(config_file);
+
+//assign the values
+const app_secret = config_values.appSecret;
+const validation_token = config_values.validationToken;
+const page_access_token = config_values.pageAccessToken;
+const server_url = config_values.serverURL;
+
+exports.page_access_token = page_access_token;
+
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log(" ");
+
+
+//Routes
+app.get('/', function(req, res){
+	res.send("hi, I am a fleeky chatbot");
+});
+
+app.get('/webhook', function(req, res){
+	if(req.query['hub.verify_token'] === validation_token ){
+		console.log("Token Validated");
+		res.status(200).send(req.query['hub.challenge'])
+	}
+	else{
+		console.log("Failed token validation, make sure the tokens match");
+		res.status(403).send("Wrong token");
+	}
+});
+
+//messages.addGetStartedButton();
+//messages.removeGetStartedButton();
+
+app.post('/webhook', function(req, res) {
+
+	var messaging_events = req.body.entry[0].messaging;
+
+	for(let i = 0; i < messaging_events.length; i++) {
+		var event = messaging_events[i];
+		var sender = event.sender.id;
+		if(event.message && event.message.text){
+			var text = event.message.text;
+			console.log(text);
+			//messages.sendText(sender, "Text echo: "+ text.substring(0, 100));
+			messages.decideResponse(sender, text);
+		}
+		if(event.postback){
+			var text = JSON.stringify(event.postback.payload);
+			console.log(text);
+			messages.decideResponse(sender,text);
+			continue;
+		}
+	}
+
+	res.sendStatus(200);
+});
+
+messages.addPersistentMenu();
+//messages.removePersistentMenu();
+
+/*function sendText(sender, text){
+	var messageData = {text: text};
+	//console.log(messageData);
+	sendRequest(sender, messageData);
+}
+
+sendRequest: function(sender, messageData){
+	request({
+		url: 'https://graph.facebook.com/v2.8/me/messages',
+		qs: {access_token : page_access_token},
+		method: 'POST',
+		json: {
+			recipient: {id : sender},
+			message: messageData
+		}
+	}, function(error, response){
+		if(error){
+			console.log("sending an error");
+		}else if(response.body.error){
+			console.log("response body error");
+		}
+	});
+}
+*/
+
+app.listen(app.get('port'), function(){
+	console.log("running: port " + app.get('port'));
+});
---
+++ messages.js
@@ -0,0 +1,195 @@
+'use strict'
+
+const request = require('request');
+const index = require('./index');
+
+module.exports = {
+	sendText: function(sender, text){
+		var messageData = {text: text};
+		//console.log(messageData);
+		sendRequest(sender, messageData);
+	},
+
+	decideResponse: function(sender, text1){
+		var text = text1.toLowerCase();
+		
+		if(text.includes("hello") || text.includes("hi")|| text.includes("hey")){
+			/* Link Account if not already linked */
+			console.log("args " + process.argv[1], process.argv[3]);
+			console.log("pwd " + process.env.PWD)
+
+		}
+		else if(text.includes("greeting")){
+			this.sendText(sender, "Account would be linked at this step." )
+		}
+		else{
+			this.sendText(sender, "Text echo: "+ text.substring(0, 100));
+		}
+	},
+
+	addPersistentMenu: function(){
+		this.addGetStartedButton();
+		request({
+			url: "https://graph.facebook.com/v2.8/me/messenger_profile",
+			qs: {access_token: index.page_access_token},
+			method: "POST",
+			json:{
+				persistent_menu : [
+				{
+					locale : "default",
+					composer_input_disabled : false,
+					call_to_actions : [
+						{
+							type: "nested",
+							title: "Make A Transaction",
+							call_to_actions : [
+								{
+									type: "postback",
+									title: "Purchase Airtime",
+									payload: "AIRTIME_PURCHASE"
+								},
+								{
+									type: "postback",
+									title: "Transfer Money",
+									payload: "TRANSFER"
+								}
+							]
+						},
+						{
+							type: "postback",
+							title: "Balance Check",
+							payload: "CHECK_BALANCE"
+						},
+						{
+							type: "postback",
+							title: "Manage Account Alerts",
+							payload: "MANAGE_ALERTS"
+						}
+					]
+				}]
+			}
+		}, function(error, response, body) {
+			console.log("adding menu" + response);
+			if(error){
+				console.log('sending error');
+				console.log('Error sending messages: ', error);
+			}else if(response.body.error){
+				console.log('response body error');
+				console.log('Error: ', response.body.error);
+			}
+		});
+	},
+
+	removePersistentMenu: function(){
+ 		request({
+    		url: 'https://graph.facebook.com/v2.8/me/messenger_profile',
+    		qs: { access_token: index.page_access_token },
+    		method: 'DELETE',
+    		json:{
+        		fields : [
+        			"persistent_menu"
+        		]
+    		}
+
+		}, function(error, response, body) {
+    		console.log("removing menu" + response.body);
+    		if (error) {
+    			console.log('sending error')
+       			console.log('Error sending messages: ', error);
+    		} else if (response.body.error) {
+    			console.log('response body error');
+        		console.log('Error: ', response.body.error);
+    		}
+		});
+ 	},
+
+ 	addGetStartedButton: function(){
+ 		request({
+    		url: 'https://graph.facebook.com/v2.8/me/messenger_profile',
+    		qs: { access_token: index.page_access_token },
+    		method: 'POST',
+    		json:{
+        		get_started : {
+        			payload : "GREETING"
+        		}
+    		}
+		}, function(error, response, body) {
+    		console.log("adding get started button" + response);
+    		if (error) {
+    			console.log('sending error')
+       			console.log('Error sending messages: ', error);
+    		} else if (response.body.error) {
+    			console.log('response body error');
+        		console.log('Error: ', response.body.error);
+    		}
+		});
+ 	}, 
+
+ 	removeGetStartedButton: function(){
+ 		request({
+    		url: 'https://graph.facebook.com/v2.8/me/messenger_profile',
+    		qs: { access_token: index.page_access_token },
+    		method: 'DELETE',
+    		json:{
+        		fields : [
+        			"get_started"
+        		]
+    		}
+
+		}, function(error, response, body) {
+    		console.log("removing get started button" + response.body);
+    		if (error) {
+    			console.log('sending error')
+       			console.log('Error sending messages: ', error);
+    		} else if (response.body.error) {
+    			console.log('response body error');
+        		console.log('Error: ', response.body.error);
+    		}
+		});
+ 	}
+
+
+
+};
+
+function sendLinkingMessage(sender, text){
+	var messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [{
+						"title":"Welcome to Bank 737",
+						"subtitle": "Simple Banking",
+						"image_url":"http://thengmag.com/blogimages/gtbank-lead.jpg",
+						"buttons":[{
+							"type": "account_link",
+							"url": "",
+							"title": "Link Account"
+						}]
+					}
+				]
+			}
+		}
+	}
+
+	sendRequest(sender, messageData);
+}
+
+function sendRequest(sender, messageData){
+	request({
+		url: 'https://graph.facebook.com/v2.8/me/messages',
+		qs: {access_token : index.page_access_token},
+		method: 'POST',
+		json: {
+			recipient: {id : sender},
+			message: messageData
+		}
+	}, function(error, response){
+		if(error){
+			console.log("sending an error");
+		}else if(response.body.error){
+			console.log("response body error");
+		}
+	});
+}
